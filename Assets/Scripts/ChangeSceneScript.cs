616c86c5e3553e07ca5039e2144e41a8b28e2798
Hannah Lövberg <hannah.lovberg@live.com>
2017-11-08 20:17:58 +0100

    Skylt och Nya Scener
    Gjorde scener och triggers för tema 2 och 3.
    Gjorde en skylt!

--- Assets/Scripts/ChangeSceneScript.cs
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -40,28 +40,81 @@ public class ChangeSceneScript : MonoBehaviour
 
     public void triggered(Collider col)
     {
-        Debug.Log(col.gameObject.tag);
+        switch(col.gameObject.tag)
+        {
+            //vecka1
+            case "Hannah1":
+                OnLoadScene(2);
+                break;
+            case "Sofi1":
+                OnLoadScene(3);
+                break;
+            case "Anneli1":
+                OnLoadScene(4);
+                break;
+            case "Tove1":
+                OnLoadScene(5);
+                break;
+            case "Lotta1":
+                OnLoadScene(6);
+                break;
 
-        if (col.gameObject.tag == "Hannah1")
-        {
-            OnLoadScene(2);
+            //vecka2
+            case "Hannah2":
+                OnLoadScene(7);
+                break;
+            case "Sofi2":
+                OnLoadScene(8);
+                break;
+            case "Anneli2":
+                OnLoadScene(9);
+                break;
+            case "Tove2":
+                OnLoadScene(10);
+                break;
+            case "Lotta2":
+                OnLoadScene(11);
+                break;
+
+                //Vecka3
+            case "Hannah3":
+                OnLoadScene(12);
+                break;
+            case "Sofi3":
+                OnLoadScene(13);
+                break;
+            case "Anneli3":
+                OnLoadScene(14);
+                break;
+            case "Tove3":
+                OnLoadScene(15);
+                break;
+            case "Lotta3":
+                OnLoadScene(16);
+                break;
         }
-        if (col.gameObject.tag == "Sofi1")
-        {
-            OnLoadScene(3);
-        }
-        if (col.gameObject.tag == "Anneli1")
-        {
-            OnLoadScene(4);
-        }
-        if (col.gameObject.tag == "Tove1")
-        {
-            OnLoadScene(5);
-        }
-        if (col.gameObject.tag == "Lotta1")
-        {
-            OnLoadScene(6);
-        }
+
+
+        //if (col.gameObject.tag == "Hannah1")
+        //{
+        //    OnLoadScene(2);
+        //}
+        //if (col.gameObject.tag == "Sofi1")
+        //{
+        //    OnLoadScene(3);
+        //}
+        //if (col.gameObject.tag == "Anneli1")
+        //{
+        //    OnLoadScene(4);
+        //}
+        //if (col.gameObject.tag == "Tove1")
+        //{
+        //    OnLoadScene(5);
+        //}
+        //if (col.gameObject.tag == "Lotta1")
+        //{
+        //    OnLoadScene(6);
+        //}
     }
 
     public void OnLoadScene(int sceneIndex)
