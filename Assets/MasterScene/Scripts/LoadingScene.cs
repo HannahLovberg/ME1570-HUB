449629421be0b28eef65bbd8c7327de84764527b
Charlotta <charlottalind_@hotmail.com>
2017-11-16 16:05:24 +0100

    Put piano sign into masterscene

---
+++ Assets/MasterScene/Scripts/LoadingScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField]
+    private Text loadingText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        int nextSceneIndex = 0;
+        int prevSceneIndex = 0;
+        ChangeSceneScript helper = FindObjectOfType<ChangeSceneScript>();
+
+        if (helper != null)
+        {
+            nextSceneIndex = helper.NextSceneIndex;
+            prevSceneIndex = helper.PrevSceneIndex;
+        }
+
+        StartCoroutine(LoadNextScene(nextSceneIndex, prevSceneIndex));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (loadingText != null)
+        {
+            Color currentColor = loadingText.color;
+            currentColor.a = Mathf.PingPong(Time.time, 1);
+            loadingText.color = currentColor;
+        }
+    }
+
+    IEnumerator LoadNextScene(int nextSceneIndex, int previousScene)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(nextSceneIndex);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }  
+    }
+}
