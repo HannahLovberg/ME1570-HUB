c8258acb4daeda371d79a126f1198762dcc29b34
Sofi Hultin <33319788+SofiHultin@users.noreply.github.com>
2017-11-16 19:05:19 +0100

    SofisSpelIgen

---
+++ Assets/Vecka 1/Sofi/Scripts/ChangeScene5.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene5 : MonoBehaviour
+{
+    public void Changemenuscene(string EuropeOrAfrica)
+    {
+        SceneManager.LoadScene(EuropeOrAfrica);
+    }
+}
