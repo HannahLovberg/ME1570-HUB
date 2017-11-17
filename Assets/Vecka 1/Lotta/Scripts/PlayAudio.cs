1125e9d74150d30c811268c646eca41566393a4d
Charlotta <charlottalind_@hotmail.com>
2017-11-17 20:07:19 +0100

    Remade my own game

---
+++ Assets/Vecka 1/Lotta/Scripts/PlayAudio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour{
+
+	AudioSource ac;
+	void Start (){
+		ac = GetComponent<AudioSource> ();
+	}
+	void OnMouseDown(){
+		ac.PlayOneShot (ac.clip);
+	}
+}
