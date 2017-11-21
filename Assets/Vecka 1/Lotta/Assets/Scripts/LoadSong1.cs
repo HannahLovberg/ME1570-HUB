376400f036600ce6085bbe0a41d577a81d21e1d9
Charlotta <charlottalind_@hotmail.com>
2017-11-21 13:11:23 +0100

    Added menu buttons

---
+++ Assets/Vecka 1/Lotta/Assets/Scripts/LoadSong1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSong1 : MonoBehaviour
+{
+	public void Changemenuscene(string Song1)
+	{
+		SceneManager.LoadScene(Song1);
+	}
+}
